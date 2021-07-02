4c8cc9aa0b6dbfd12ffc861f0bcc077c20581bdd
phu0n9 <pnha1303@gmail.com>
2021-07-02 17:25:06 +0700

    update UI

--- client/src/Setting.js
+++ client/src/Setting.js
@@ -4,7 +4,12 @@ export default function Setting() {
     return (
         <span className="setting-wrapper">
             <img src="black-bell.png" alt="noti" className="img-wrapper"/>
-            <img src="black-user.png" alt="user" className="img-wrapper"/>
+            <div class="dropdown">
+                <img src="black-user.png" alt="user" className="img-wrapper"/>
+                <div class="dropdown-content">
+                    <p>Log out</p>
+                </div>
+            </div>
             <img src="black-settings.png" alt="setting" className="img-wrapper"/>
         </span>
     )
